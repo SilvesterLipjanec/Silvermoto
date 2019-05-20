c5049d630b4507154b2b83fb825d195f9644d3cf
Silvester Lipjanec <xlipja01@stud.fit.vutbr.cz>
2019-05-20 22:53:18 +0200

    relative positioning of header

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
-(function(){
-
-    var parallax = document.querySelectorAll(".slower-move"),
-        speed = 0.5;
-
-    window.onscroll = function(){
-      [].slice.call(parallax).forEach(function(el,i){
-
-        var windowYOffset = window.pageYOffset,
-            elBackgrounPos = "-" + (windowYOffset * speed) + "px";
-
-        el.style.top = elBackgrounPos;
-
-      });
-    };
-
-  })();
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementsByClassName("navbar")[0].style.top = "0";
+  } else {
+    document.getElementsByClassName("navbar")[0].style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
